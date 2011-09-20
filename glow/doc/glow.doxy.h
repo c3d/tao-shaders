84450923e513706166ac6dc3ace88deb825615cb
Soulisse Baptiste <soulisse@polytech.unice.fr>
2011-09-20 14:07:01 +0200

    Add template and documentation for glow module. Closes #1397.

---
+++ glow/doc/glow.doxy.h
@@ -0,0 +1,34 @@
+/**
+ * @defgroup Glow Glow
+ * @ingroup Modules
+ *
+ * Generate a glow effect on the current picture.
+ *
+ * This module allows you to add some glow on the current texture.
+ * The following code shows an example of this module.
+ * @include glow.ddd
+ *
+ * @image html glow.png "Output from glow.ddd"
+ * @{
+ */
+
+/**
+ * Applies some radial glow on the current picture.
+ *
+ * Add glow on the current texture. This effect is generated thanks to a radial blur on each pixel.
+ *
+ * @param n number of the samples of the glow
+ * @param l length of the glow
+ * @param g ratio of the glow
+ * @param s ratio of the shadow
+ * @param w width of the picture
+ * @param h height of the picture
+ *
+ * @note The more the number of samples is raised, the more the quality of the glow increases, on the contrary of the performances.
+ */
+radial_glow(n:real, l:real, g:real, s:real, w:real, h:real);
+
+/**
+/**
+* @}
+*/
