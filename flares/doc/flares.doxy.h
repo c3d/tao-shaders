f75973887160c8af5f27d9de570058195945b2c2
Christophe de Dinechin <christophe@taodyne.com>
2011-11-21 17:47:00 +0100

    Updated xl.syntax and added C.syntax, fix #1651, refs #1652.
    Notice that this requires the latest version of XLR to work
    (also committed with it).

--- flares/doc/flares.doxy.h
+++ flares/doc/flares.doxy.h
@@ -1,18 +1,11 @@
 /**
- * @~english
- * @taomoduledescription{Flares, Flares shader}
+ * @defgroup Flares Flares shader
+ * @ingroup Modules
  *
  * GLSL fragment shader to generate a changing picture.
  *
+ * 
  * An example:
- * @~french
- * @taomoduledescription{Flares, Effet "Flares"}
- *
- * Un <em>fragment shader</em> GLSL qui génère des formes géométriques
- * changeantes.
- *
- * Un example:
- * @~
  @code
 import Flares
 
@@ -34,15 +27,13 @@ animated_star N:integer -> animated_star N, N
 animated_star Nb
  @endcode
  *
- * @image html "flares_example.png" "Flares"
+ * @image html "flares_example.png" "Example"
  *
- * @endtaomoduledescription{Flares}
  * @{
  */
 
 
 /**
- * @~english
  * Create the Flares texture
  *
  * The picture changes as the value of @p p changes.
@@ -52,17 +43,6 @@ animated_star Nb
  * @p saturation is a positive real value to control the color. With
  * 0.0, you get a black-and-white picture. With 1.0 or 2.0 you get
  * a much more colorful picture.
- *
- * @~french
- * Crée la texture "Flares"
- *
- * L'image change pour chaque valeur de @p p. Vous pouvez par exemple
- * utiliser l'heure ou la position de la souris comme base pour la valeur
- * de @p p.
- *
- * @p saturation est une valeur réelle positive qui permet de contrôler
- * la saturation de la couleur. 0.0 donne une image en noir et blanc, avec
- * 1.0 ou 2.0 vous obtenez une image riche en couleurs.
  */
 flares(p:real, saturation:real);
 
--- glow/doc/glow.doxy.h
+++ glow/doc/glow.doxy.h
@@ -1,6 +1,6 @@
 /**
- * @~english
- * @taomoduledescription{Glow, Glow}
+ * @defgroup Glow Glow
+ * @ingroup Modules
  *
  * Generate a glow effect on the current picture.
  *
@@ -8,29 +8,11 @@
  * The following code shows an example of this module.
  * @include glow.ddd
  *
- * @image html glow.png "The glow effect"
- * @endtaomoduledescription{Glow}
- *
- * @~french
- * @taomoduledescription{Glow, Effet néon}
- *
- * Ajoute un effet néon sur une image.
- *
- * Grâce à ce module, vous pouvez ajouter un effet de lumière sur la texture
- * en cours. Les parties les plus claires apparaîssent comme si elles
- * émettaient de la lumière (halo lumineux).
- * Voici une illustration de cet effet.
- * @include glow.ddd
- *
- * @image html glow.png "L'effet néon"
- * @endtaomoduledescription{Glow}
- *
- * @~
+ * @image html glow.png "Output from glow.ddd"
  * @{
  */
 
 /**
- * @~english
  * Applies some radial glow on the current picture.
  *
  * Add glow on the current texture. This effect is generated thanks to a radial blur on each pixel.
@@ -43,25 +25,6 @@
  * @param h height of the picture
  *
  * @note The more the number of samples is raised, the more the quality of the glow increases, on the contrary of the performances.
- *
- * @~french
- * Applique un effet de rayonnement radial sur la texture courante.
- *
- * L'effet de lumière est créé en floutant l'image puis en superposant
- * partiellement l'image floutée et l'image originale. Les paramètre de cette
- * primitive contrôlent comment le floutage est fait (@p n, @p l, @p w, @p h)
- * et comment il est mélangé à l'image (@p g, @p s). Pour un bon résultat,
- * utiliser une fraction de la taille de la texture pour @p w et @p h, comme
- * exemple (@p texture_width / 8) et (@p texture_height / 8).
- * La finesse du halo est meilleure si @p n est élevé, mais augmenter ce
- * paramètre demande plus de temps de calcul.
- *
- * @param n Nombre de points à utiliser autour de chaque pixel pour créer le flou.
- * @param l Ampleur du halo.
- * @param g Quantité de halo dans l'image finale (0.0 à 1.0).
- * @param s Quantité de l'image originale dans l'image finale (0.0 à 1.0).
- * @param w Largeur.
- * @param h Hauteur.
  */
 radial_glow(n:real, l:real, g:real, s:real, w:real, h:real);
 
