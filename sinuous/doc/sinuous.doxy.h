c000509aba16022d64f86886d81019de143cb326
Jerome Forissier <jerome@taodyne.com>
2012-01-11 17:54:42 +0100

    Translate documentation of Sinuous module into french.

--- sinuous/doc/sinuous.doxy.h
+++ sinuous/doc/sinuous.doxy.h
@@ -1,4 +1,5 @@
 /**
+ * @~english
  * @taomoduledescription{Sinuous, Sinuous}
  *
  * Generate a changing ribbon.
@@ -6,21 +7,36 @@
  * This module allows you to create a changing picture with some sinuous effects.
  *
  * The following code shows an example of this module.
+ *
+ * @~french
+ * @taomoduledescription{Sinuous, Effet "Sinuous"}
+ *
+ * Un <em>fragment shader</em> GLSL qui génère une image avec des formes
+ * ondulantes.
+ *
+ * Un exemple :
+ * @~
  * @include sinuous.ddd
  *
- * @image html sinuous.png "Output from sinuous.ddd"
+ * @image html sinuous.png "Sinuous"
  *
  * @endtaomoduledescription{Sinuous}
  * @{
  */
 
 /**
-* Draw sinuous shape.
-*
-* Draw four interconnected sinuous shape at the given coordinates.
-* @a X and @a Y indicate the center of the shape.
-* @a W and @a H are its dimensions.
-**/
+ * @~english
+ * Draw sinuous shape.
+ *
+ * Draw four interconnected sinuous shape at the given coordinates.
+ * @a X and @a Y indicate the center of the shape.
+ * @a W and @a H are its dimensions.
+ *
+ * @~french
+ * Affiche l'image "Sinuous"
+ *
+ * @a X et @a Y est le centre de l'image. Ses dimensions sont @a W et @a H.
+ **/
 sinuous_shape(X:real, Y:real, W:real, H:real);
 
 /**
